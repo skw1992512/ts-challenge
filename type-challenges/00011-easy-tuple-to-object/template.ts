817c88f777672b1a40387a38f4767931bb7f6a4c
a470555883 <470555883@qq.com>
2022-05-22 01:45:11 +0800

    11 finnish

---
+++ type-challenges/00011-easy-tuple-to-object/template.ts
@@ -0,0 +1,15 @@
+type TupleToObject<T extends readonly any[]> = {
+  [P in T[number]]: P;
+};
+
+//js
+function TupleToObject(array) {
+  const obj = {};
+  array.foreach((val) => {
+    obj[val] = val;
+  });
+  return obj;
+}
+
+//1.返回一个对象
+//2.遍历数组
